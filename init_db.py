d4bf26d87184939a8803edc73495dd5c6e2812d2
Dogukan Aksoy <dgnkaksy06@gmail.com>
2026-02-28 13:24:42 +0300

    db initialized

---
+++ init_db.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+from flask_app.config import Config
+
+def init_db():
+    try:
+        conn = psycopg2.connect(Config.DATABASE_URL)
+
+        cur = conn.cursor()
+
+        # Create `telemetry` table
+        cur.execute("DROP TABLE IF EXISTS telemetries;")
+        cur.execute("CREATE TABLE telemetries (id SERIAL PRIMARY KEY," \
+                                            "sensor_id VARCHAR(50) NOT NULL," \
+                                            "temperature DOUBLE PRECISION NOT NULL," \
+                                            "recorded_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);"
+                                            )
+        
+        # Add an example rcord
+        cur.execute("INSERT INTO telemetries (sensor_id, temperature) VALUES (%s, %s)", ("room_24", 22.5))
+        
+        conn.commit()
+        
+    except Exception as ex:
+        print(f"An error occured while running `init_db()` {ex}")
+    finally:
+        cur.close()
+        conn.close()
+
+if __name__ == "__main__":
+    init_db()
